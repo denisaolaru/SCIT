241e87a6df2fc523449e779cfb80cc05b7e60ed2
Olaru Denisa <olarudenisa02@gmail.com>
2019-06-12 20:23:14 +0300

    removing ignored files

--- OOP/src/helloworld.java
+++ OOP/src/helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
-        System.out.println("hello");
+        System.out.println("Hello");
+        System.out.println("Hi");
     }
 }
